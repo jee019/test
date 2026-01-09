38a079eebed6d474403739e96f56f6cd7247337d
jee019 <qwer010910@gmail.com>
2026-01-09 11:01:42 +0000

    docs

--- render_mos.py
+++ render_mos.py
@@ -1,23 +1,26 @@
+#!/usr/bin/env python3
 """Generate forms for human evaluation."""
 
 from jinja2 import FileSystemLoader, Environment
-from utils import QuestionGenerator_PMOS as QuestionGenerator
+from utils import QuestionGenerator_MOS as QuestionGenerator
 
 
 def main():
     """Main function."""
     loader = FileSystemLoader(searchpath="./templates")
     env = Environment(loader=loader)
-    template = env.get_template("pair_comparison.html.jinja2")
+    template = env.get_template("mos.html.jinja2")
+    
+
 
     html = template.render(
-        page_title="Emotion Perception Experiment Form 1", 
+        page_title="MOS Experiment Form 1",
         form_id=1,
-        type="s-MOS",
-        questions = QuestionGenerator("filelist/s-mos.csv").questions
+        type="q-MOS",
+        questions = QuestionGenerator("filelist/audio_order.csv").questions
     )
-
-    with open("smos/rendered_pair_comparison.html", "w") as f:
+    # write html into file
+    with open("docs/mos.html", "w") as f:
         f.write(html)
         print("Done!")
 
