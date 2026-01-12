a720a5c86721c06f0664d6e4c1f425e286710725
jee019 <qwer010910@gmail.com>
2026-01-12 11:05:34 +0000

    combine

---
+++ render_eval.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Generate combined MOS & SMOS forms for human evaluation."""
+
+from jinja2 import FileSystemLoader, Environment
+from utils import QuestionGenerator_MOS, QuestionGenerator_SMOS
+
+
+def main():
+    """Main function."""
+    loader = FileSystemLoader(searchpath="./templates")
+    env = Environment(loader=loader)
+    template = env.get_template("eval.html.jinja2")
+    
+    # Load MOS questions
+    mos_questions = QuestionGenerator_MOS("filelist/MOS.csv").questions
+    
+    # Load SMOS questions
+    smos_questions = QuestionGenerator_SMOS("filelist/SMOS.csv").questions
+
+    html = template.render(
+        page_title="MOS & SMOS Experiment Form",
+        form_url="https://script.google.com/macros/s/AKfycbz88pG3Usw7jdtNWh9pUWnfPR855SESSYZFGyaYbPv006CSZplhdrc2v2wwhjakF8mkjg/exec",  # MOS용 Google Apps Script URL
+        mos_questions=mos_questions,
+        smos_questions=smos_questions
+    )
+    
+    # Write html into file
+    with open("eval.html", "w", encoding="utf-8") as f:
+        f.write(html)
+        print("Done! Generated eval.html")
+
+
+if __name__ == "__main__":
+    main()
